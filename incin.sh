6d210192d6a6105ca2f8a395018ba9842276481a
Sam Jarman <samjarman171@gmail.com>
2014-01-12 13:31:22 +1300

    Adds history

--- incin.sh
+++ incin.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 echo "Welcome to IncIn, an inline interpreter for c";
-
+CODE=""
 while true; do
-	read -p ">>> " code
+	read -p ">>> " temp
+	CODE="$CODE $temp" #Append old code to new
 
 	#Write out file
 	echo "#include <stdio.h>" > temp.c;
 	echo "int main(void) {" >> temp.c;
-		echo $code >> temp.c;
+		echo $CODE >> temp.c;
 	echo "return 0;}" >> temp.c;
 	
 	#compile
@@ -16,9 +17,8 @@ while true; do
 	
 	#run
 	./temp;
-	
 	#cleanup
-	rm temp.c;
-	rm ./temp;
+	#rm temp.c;
+	#rm ./temp;
 	echo
 done
